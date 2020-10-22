79f9dded9a8a0be56114ab5e4d6e34e808b6aa5d
berosoboy <andrelvsousa@gmail.com>
2020-10-22 00:05:19 -0300

    Some adjusts on PlayerO animation

--- player_o.gd
+++ player_o.gd
@@ -1,22 +1,26 @@
 tool
 extends Node2D
 
-var end_angle := 0.0
-export var draw_velocity := 5.0
+export var draw_velocity := 6.28
 export var radius := 16.0
 export var width := 4.0
 export var color := Color.aqua
 
-func _process(delta):
-	if end_angle < TAU:
-		end_angle += (delta * draw_velocity)
-		update()
-	elif Engine.editor_hint:
-		end_angle = 0.0
+var end_angle := 0.0
+
+func _process(delta) -> void:
+	if end_angle > TAU:
+		if Engine.editor_hint:
+			end_angle = 0.0
+		else:
+			set_process(false)
+		return
+	end_angle += (delta * draw_velocity)
+	update()
 
 
-func _draw():
+func _draw() -> void:
 	var start_angle := 0.0
 	var point_count := 60
 	var antialiased := true
-	draw_arc(Vector2.ZERO, radius, start_angle, end_angle, point_count, color, width, antialiased)
+	draw_arc(Vector2.ZERO, radius + width, start_angle, end_angle, point_count, color, width, antialiased)
